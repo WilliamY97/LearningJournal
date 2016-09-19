a73070b0f86fee77c3e85b3701e198fb3983441e
William Yang <yangwilliam92@gmail.com>
2016-09-19 00:36:18 -0400

    add

--- CrackingTheCodeInterview/Arrays & Strings/1.1.py
+++ CrackingTheCodeInterview/Arrays & Strings/1.1.py
@@ -1,15 +1,32 @@
 #1.1 Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures 
 
-"""MY SOLUTION WITH ADDITIONAL DATA STRUCTURES: I write a function that accepts a string. A for loop is implemented to run through the string by letter and compare it with the check string. If the contents of check is the same as the current letter from the loop then it must be not unique. However if the loop runs through the entire string without hitting this case - then it is a unique string."""
+# With data structures
+
+#SET
+"""This implementation runs with the set data structure which
+can only have distinct values in it. So I've added letters into
+the set as the for loop runs and if a letter matches a letter already
+in the set then it isn't unique. """
+def is_unique2(x):
+	seen = set ()
+	for letter in x:
+		if letter in seen:
+			return False
+		seen.add(letter)
+	return True
+
+# Without data structures
+
+"""For every letter in the string x I check if the 
+letter exists more than once with the .count() function.
+This runs in quadratic time as the count itself must iterate
+through the entire string."""
 
 def is_unique(x):
-    check = ''
-    for letter in x:
-        if letter == check:
-            return False
-        else:
-            check = letter
-    return True
+	for letter in x:
+		if x.count(letter) > 1:
+			return False
+	return True
 
 """test cases"""
 
