a4f69af360d5b59643c12a0c518768bcf6e127b5
William Yang <yangwilliam92@gmail.com>
2016-09-23 12:25:54 -0400

    Update 1.3.py

--- CrackingTheCodeInterview/Arrays & Strings/1.3.py
+++ CrackingTheCodeInterview/Arrays & Strings/1.3.py
@@ -1,1 +1,17 @@
+def URLify (s,i):
+    new = ''
+    for letter in s:
+        i -= 1
+        if i < 0:
+            break
+        elif (letter == ' '):
+            new += ('%20')
+        else:
+            new += (letter)
+    return new
 
+print URLify('Mr John Smith    ',13)
+print URLify('much ado about nothing      ', 22)
+
+#Mr%20John%20Smith 
+#much%20ado%20about%20nothing 
