c38e7f031bf2f8500a79a5b569c21ffb0a3f5464
William Yang <yangwilliam92@gmail.com>
2016-09-25 11:50:07 -0400

    Update 2.1_RemoveDups.py

--- CrackingTheCodeInterview/Linked Lists/2.1_RemoveDups.py
+++ CrackingTheCodeInterview/Linked Lists/2.1_RemoveDups.py
@@ -1,11 +1,23 @@
-def RemoveDups(ll):
-    current = ll.next
-    seen = set(current.value)
-    
-    while ll.next != None:
-        if ll.next in seen:
-            ll.next = ll.next.next
+from LinkedList import LinkedList
+
+def remove_dups(ll):
+    if ll.head is None:
+        return
+
+    current = ll.head
+    seen = set([current.value])
+    while current.next:
+        if current.next.value in seen:
+            current.next = current.next.next
         else:
-            seen.add(ll.next)
-    
+            seen.add(current.next.value)
+            current = current.next
+
     return ll
+
+ll = LinkedList()
+ll.generate(10, 0, 9)
+print(ll)
+remove_dups(ll)
+print(ll)
+
