26316c76345baa57b2741bf77f362e2a7b137ef3
William Yang <yangwilliam92@gmail.com>
2017-02-10 19:18:32 -0500

    added binary search

--- Algorithms & Data Structures/Python Data Structure Implementations/binarytree.py
+++ Algorithms & Data Structures/Python Data Structure Implementations/binarytree.py
@@ -14,15 +14,43 @@ def insertNode(root,key):
 		if root.left: insertNode(root.left,key)
 		else: root.left = Node(key)
 
+def deleteNode(root,key):
+	if root is None: return None
+
+def binarySearch(root,key):
+	if root is None: return None
+	if root.val == key:
+		print root.val
+		return root
+	elif key > root.val:
+		binarySearch(root.right,key)
+	else:
+		binarySearch(root.left,key)
+
 def inOrderSearch(root):
 	if root is None: return None
 	inOrderSearch(root.left)
 	print root.val
 	inOrderSearch(root.right)
 
+def findMin(node):
+	while node.left is not None: node = node.left
+	return node
+
+def successor(node):
+	if node is None: return None
+	print findMin(node.right).val
+
 tree = Node(5)
 insertNode(tree,7)
 insertNode(tree,3)
+insertNode(tree,8)
 insertNode(tree,9)
 
+print "Performing Binary Search..."
+binarySearch(tree,9)
+
+print "Performing In Order Search..."
 inOrderSearch(tree)
+print "Performing Successor..."
+successor(tree)
