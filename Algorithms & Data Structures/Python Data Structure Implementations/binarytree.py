963652cae5c9607514dd18654d3c8a88fcc47aa9
William Yang <yangwilliam92@gmail.com>
2017-02-12 20:05:41 -0500

    added height check for binary tree

--- Algorithms & Data Structures/Python Data Structure Implementations/binarytree.py
+++ Algorithms & Data Structures/Python Data Structure Implementations/binarytree.py
@@ -17,6 +17,10 @@ def insertNode(root,key):
 def deleteNode(root,key):
 	if root is None: return None
 
+def height(root):
+	if root is None: return 0
+	else: return max(height(root.left),height(root.right)) + 1
+
 def binarySearch(root,key):
 	if root is None:
 		print "Nothing Found."
@@ -73,3 +77,6 @@ preOrderSearch(tree)
 
 print "Performing Successor..."
 successor(tree)
+
+print "Finding Height of Tree..."
+print height(tree)
