387f2b5532820db4732343761af8ad31b45428fb
William Yang <yangwilliam92@gmail.com>
2017-02-20 01:10:48 -0500

    Added linked list data structure

---
+++ Algorithms & Data Structures/Python Data Structure Implementations/linkedlist.py
@@ -0,0 +1,44 @@
+class Node :
+	def __init__( self, data ) :
+		self.data = data
+		self.next = None
+		self.prev = None
+
+class LinkedList :
+	def __init__( self ) :
+		self.head = None
+
+	def add( self, data ) :
+		node = Node( data )
+		if self.head == None :
+			self.head = node
+		else :
+			node.next = self.head
+			node.next.prev = node
+			self.head = node
+
+def has_cycle(head):
+    slow = head
+    fast = head
+
+    while fast != None:
+        slow = slow.next
+
+        if fast.next != None:
+            fast = fast.next.next
+        else:
+            return False
+
+        if slow is fast:
+            return True
+    return False
+
+l = LinkedList()
+
+l.add( 'a' )
+l.add( 'b' )
+l.add( 'c' )
+
+l.head.next.next.next = l.head
+
+print has_cycle(l.head)
