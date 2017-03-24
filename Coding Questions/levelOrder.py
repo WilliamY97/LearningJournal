2322bdd614a7bb3df0b5ee5d222de3e8a0f4a1ab
William Yang <yangwilliam92@gmail.com>
2017-03-23 23:20:30 -0400

    Update levelOrder.py

--- Coding Questions/levelOrder.py
+++ Coding Questions/levelOrder.py
@@ -1,8 +1,3 @@
-# level is a list of the nodes in the current level.
-# Keep appending a list of the values of these nodes to ans and then updating level with all the nodes in
-# the next level (kids) until it reaches an empty level. Python's list comprehension makes it easier to deal
-#  with many conditions in a concise manner.
-
 def levelOrder(self, root):
     if not root: return []
     ans,level = [],[root]
