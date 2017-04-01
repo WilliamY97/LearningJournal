e19ade955da09c4169657fb414b824a4cb4b81fc
William Yang <yangwilliam92@gmail.com>
2017-04-01 15:55:57 -0400

    add

--- Coding Questions/zigzagLevelOrder.py
+++ Coding Questions/zigzagLevelOrder.py
@@ -1,12 +1,12 @@
 def zigzagLevelOrder(self, root):
     if not root: return []
-    res, temp, stack, flag = [], [], [root], 1
-    while stack:
-        for i in range(0,len(stack)):
-            node = stack.pop(0)
+    res, temp, queue, flag = [], [], [root], 1
+    while queue:
+        for i in range(0,len(queue)):
+            node = queue.pop(0)
             temp += [node.val]
-            if node.left: stack +=[node.left]
-            if node.right: stack +=[node.right]
+            if node.left: queue +=[node.left]
+            if node.right: queue +=[node.right]
         res+=[temp[::flag]]
         temp = []
         flag*= -1
