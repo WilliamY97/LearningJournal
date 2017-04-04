2be7e73264ab5aad8717f0a47ecc2bd5ea1a9f29
William Yang <yangwilliam92@gmail.com>
2017-04-04 09:32:41 -0400

    add

--- Coding Questions/RemoveDuplicatesFromSortedArray.py
+++ Coding Questions/RemoveDuplicatesFromSortedArray.py
@@ -18,3 +18,16 @@ def removeDuplicates(self, nums):
             tail += 1
             nums[tail] = nums[i]
     return tail + 1
+
+"""
+SOLUTION:
+
+Have two pointers, one fast (start one ahead), one slow. Have the fast pointer compare itself with the slow pointers value.
+If they're the same then it's a duplicate and we can ignore it and keep moving the fast pointer. If the fast compared to
+the slow is different then the slow pointer can move over one index and set that index to the value from fast. This will
+ensure that everytime a unique value is detected we can simply have the slow pointer repopulate the index from the start
+with these new values. At the end return the pointer index + 1 (because it started at 0) and the array should be formatted
+to have all the unique values in sequence from the start - with whatever buffer leftover at the end indices - but that
+doesn't matter just as the question states.
+
+"""
