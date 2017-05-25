854fa00ee40d5e04b84a48df39e1f0d8d5a102d0
William Yang <yangwilliam92@gmail.com>
2017-05-24 23:03:19 -0400

    Add Heap Data Structure

--- Algorithms and Data Structures/Heap/Heap.py
+++ Algorithms and Data Structures/Heap/Heap.py
@@ -1,1 +1,26 @@
+import math
 
+def max_heapify(A, i):
+    left = 2 * i + 1
+    right = 2 * i + 2
+    largest = i
+    if left < len(A) and A[left] > A[largest]:
+        largest = left
+    if right < len(A) and A[right] > A[largest]:
+        largest = right
+    if largest != i:
+        A[i], A[largest] = A[largest], A[i]
+        max_heapify(A, largest)
+
+def build_max_heap(A):
+    for i in range(len(A) // 2, -1, -1):
+        max_heapify(A, i)
+    return A
+
+A = [16,4,10,14,7,9,3,2,8,1]
+
+print A
+
+A = build_max_heap(A)
+
+print A
