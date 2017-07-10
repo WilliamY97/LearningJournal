c04e30a6e288236b0fab15ad8cb62e0585069606
William Yang <yangwilliam92@gmail.com>
2017-07-09 23:13:14 -0400

    Update preperation.py

--- Coding Questions/preperation.py
+++ Coding Questions/preperation.py
@@ -104,3 +104,19 @@ class Stack:
     
     def empty(self):
         return not len(self._queue)
+    
+#zigzag
+
+def zigzagLevelOrder(self, root):
+    if not root: return []
+    res, temp, stack, flag = [], [], [root], 1
+    while stack:
+        for i in range(0,len(stack)):
+            node = stack.pop(0)
+            temp += [node.val]
+            if node.left: stack +=[node.left]
+            if node.right: stack +=[node.right]
+        res+=[temp[::flag]]
+        temp = []
+        flag*= -1
+    return res
