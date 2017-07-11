5450b470fa26d5e38be2d51267124da9bf7be2c5
William Yang <yangwilliam92@gmail.com>
2017-07-11 02:14:23 -0400

    Update preperation.py

--- Coding Questions/preperation.py
+++ Coding Questions/preperation.py
@@ -1,3 +1,32 @@
+   def minWindow(self, S, T):
+        indices = {}
+        for char in T:
+            indices[char] = []
+        miss = list(T)
+        start = 0
+        end = len(S)
+        for i in range(len(S)):
+            if S[i] in T:
+                if S[i] not in miss and indices[S[i]] != []:
+                    indices[S[i]].pop(0)
+                elif S[i] in miss:
+                    miss.remove(S[i])
+                indices[S[i]].append(i)
+            if miss == []:
+                maximum = max([x[-1] for x in indices.values()])
+                minimum = min([x[0] for x in indices.values()])
+                if maximum-minimum+1 < end-start+1:
+                    start = minimum
+                    end = maximum
+        if miss != []:
+            return ""
+        else:
+            return S[start:end+1]
+        
+Basically I kept a dictionary to record the index of each character of T. Each time I found a window, (when miss == []), 
+I checked the length of this window by subtracting the maximum index and the minimum index of the characters. 
+If this window is the smallest one so far, I record its beginning and ending index as "start" and "end."
+
 # Coin Change DP O(k*n) types of money * amount [you have to iterate through each amount before and check each money for it]
 
 def coinChange(coins, amount):
