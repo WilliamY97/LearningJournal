4db0e025f1b54912d81451112344d4477cf67558
William Yang <yangwilliam92@gmail.com>
2017-07-11 01:44:46 -0400

    Update preperation.py

--- Coding Questions/preperation.py
+++ Coding Questions/preperation.py
@@ -175,7 +175,7 @@ print letterCombinations('501') -> bad values
 
 
     
-#zigzag
+# ZIG ZAG LEVEL ORDER TRAVERSAL
 
 def zigzagLevelOrder(self, root):
     if not root: return []
@@ -190,3 +190,30 @@ def zigzagLevelOrder(self, root):
         temp = []
         flag*= -1
     return res
+
+
+# TIC TAC TOE VICTORY
+
+def CheckVictory(board, x, y):
+
+    #check if previous move caused a win on vertical line 
+    if board[0][y] == board[1][y] == board [2][y]:
+        return True
+
+    #check if previous move caused a win on horizontal line 
+    if board[x][0] == board[x][1] == board [x][2]:
+        return True
+
+    #check if previous move was on the main diagonal and caused a win
+    if x == y and board[0][0] == board[1][1] == board [2][2]:
+        return True
+
+    #check if previous move was on the secondary diagonal and caused a win
+    if x + y == 2 and board[0][2] == board[1][1] == board [2][0]:
+        return True
+
+    return False  
+
+
+
+
